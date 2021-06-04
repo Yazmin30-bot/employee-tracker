8ee494c1a366aeeff27cc0e0f19f6a7f4bbf6a6c
Yazmin Tinoco <ytip30@gmail.com>
2021-06-04 18:02:22 -0500

    Create a role table on schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,13 @@ CREATE TABLE department (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
+
+--Create a role table
+CREATE TABLE role(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL  
+);
