f9735166c2f1c897ba75555cf5a7d8789cba1e97
Yazmin Tinoco <ytip30@gmail.com>
2021-06-04 18:32:27 -0500

    Create the connection with mysql

--- db/index.js
+++ db/index.js
@@ -1,3 +1,19 @@
 //Import mysql and inquirer dependencies
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+//Create the conexion with mysql
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    // Your port, if not 3306
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Be sure to update with your own MySQL password!
+    password: 'root',
+    database: 'employees',
+    multipleStatements: true,
+  });
